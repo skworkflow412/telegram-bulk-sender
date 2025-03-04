6914c15250042963fec039956936e3654b875778
skworkflow412 <skworkflow01@gmail.com>
2025-03-04 20:00:29 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-  mongoURI: process.env.MONGO_URI || "mongodb+srv://your-db-uri",
+  mongoURI: process.env.MONGO_URI || "mongodb+srv://wednesday:wednesday@wednesday.pl7um.mongodb.net/?retryWrites=true&w=majority&appName=wednesday",
   port: process.env.PORT || 8080, // Koyeb uses port 8080
 };
